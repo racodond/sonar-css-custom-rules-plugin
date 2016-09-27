4038f945d8f9aa1548f56366972f47efa3f61cbc
David RACODON <david.racodon@gmail.com>
2016-09-28 01:15:41 +0200

    Add SonarQube.com URL

--- travis.sh
+++ travis.sh
@@ -3,5 +3,5 @@
 mvn clean install
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  mvn sonar:sonar -Dsonar.login=$SONAR_TOKEN
+  mvn sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
 fi
