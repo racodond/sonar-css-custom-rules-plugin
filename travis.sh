a06c6988f5d07e737deaaec22f29fc33ccbb5507
David RACODON <david.racodon@gmail.com>
2018-03-06 16:17:25 +0100

    Test against both LTS and LATEST_RELEASE

--- travis.sh
+++ travis.sh
@@ -2,18 +2,22 @@
 
 set -euo pipefail
 
-mvn clean install
+mvn -B clean install -Pits -Dsonar.runtimeVersion=$SQ_VERSION
 
-mvnCommand='mvn -B sonar:sonar'
-commonArgs="-Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=racodond-github"
-githubArgs="-Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.oauth=$GITHUB_TOKEN"
-branchArgs=''
+if [ "$SQ_VERSION" == "LTS" ]; then
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    if [ "$TRAVIS_BRANCH" != "master" ]; then
-       branchArgs="-Dsonar.branch.name=$TRAVIS_BRANCH -Dsonar.branch.target=master"
+    mvnCommand='mvn -B sonar:sonar'
+    commonArgs="-Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.organization=racodond-github"
+    githubArgs="-Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.github.oauth=$GITHUB_TOKEN"
+    branchArgs=''
+
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        if [ "$TRAVIS_BRANCH" != "master" ]; then
+           branchArgs="-Dsonar.branch.name=$TRAVIS_BRANCH -Dsonar.branch.target=master"
+        fi
+        eval $mvnCommand $commonArgs $branchArgs
+    else
+        eval $mvnCommand $commonArgs $githubArgs
     fi
-    eval $mvnCommand $commonArgs $branchArgs
-else
-    eval $mvnCommand $commonArgs $githubArgs
+
 fi
